81ad757df05a4464a26d8927fb2128e56116743d
Harrison Itotia <harrisonitotia@gmail.com>
2024-07-02 11:32:21 +0300

    create and scaffold movie app

---
+++ MovieBlazorApp/Data/MovieBlazorAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieBlazorApp;
+
+namespace MovieBlazorApp.Data
+{
+    public class MovieBlazorAppContext : DbContext
+    {
+        public MovieBlazorAppContext (DbContextOptions<MovieBlazorAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieBlazorApp.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MovieBlazorApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using MovieBlazorApp.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieBlazorApp.Data;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MovieBlazorAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieBlazorAppContext") ?? throw new InvalidOperationException("Connection string 'MovieBlazorAppContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();;
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
