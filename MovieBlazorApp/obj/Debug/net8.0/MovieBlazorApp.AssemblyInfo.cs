5dee6cfceb22afb061c9b156ea8eb55e940a0dbd
Harrison Itotia <harrisonitotia@gmail.com>
2024-07-02 12:42:56 +0300

    authorize users

--- MovieBlazorApp/obj/Debug/net8.0/MovieBlazorApp.AssemblyInfo.cs
+++ MovieBlazorApp/obj/Debug/net8.0/MovieBlazorApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8ccedec5de05a5dca94f573219f616a3946385")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25dc06c739497abe15a1b8e29596418556afee65")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
