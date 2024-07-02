d13a148aa1a9f477c9e45740daa66b8f89b606c5
Harrison Itotia <harrisonitotia@gmail.com>
2024-07-02 11:37:49 +0300

    Add Migration

--- MovieBlazorApp/obj/Debug/net8.0/MovieBlazorApp.AssemblyInfo.cs
+++ MovieBlazorApp/obj/Debug/net8.0/MovieBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ad757df05a4464a26d8927fb2128e56116743d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
